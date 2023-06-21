7f38bd1ed66e46848b45d5692f685fe2b17b9b7c
Felipe Espinoza <domingues.espinoza@outlook.com>
2023-06-21 16:39:54 -0300

    Adicionando Tabela de Indices

--- src/components/Tabela/index.jsx
+++ src/components/Tabela/index.jsx
@@ -5,7 +5,42 @@ const Tabela = () => {
         <section>
             <div className={styles.container}>
                 <h3 className={styles.text}>Consulte o seu resultado</h3>
-                <img className={styles.img} src="../../src/assets/tabelaIMC.png" alt="" />
+                <table className={styles.table}>
+                    <thead>
+                    <tr>
+                        <th>IMC</th>
+                        <th>CLASSIFICAÇÃO</th>
+                        <th>OBESIDADE (GRAU)</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>MENOR QUE 18,5</td>
+                        <td>MAGREZA</td>
+                        <td>0</td>
+                    </tr>
+                    <tr>
+                        <td>ENTRE 18,5 E 24,9</td>
+                        <td>NORMAL</td>
+                        <td>0</td>
+                    </tr>
+                    <tr>
+                        <td>ENTRE 25,0 E 29,9</td>
+                        <td>SOBREPESO</td>
+                        <td>I</td>
+                    </tr>
+                    <tr>
+                        <td>ENTRE 30,0 E 39,9</td>
+                        <td>OBESIDADE</td>
+                        <td>II</td>
+                    </tr>
+                    <tr>
+                        <td>MAIOR QUE 40,0</td>
+                        <td>OBESIDADE GRAVE</td>
+                        <td>III</td>
+                    </tr>
+                    </tbody>
+                </table>
             </div>
         </section>
     )
